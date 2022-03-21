e040ed1402797c41672f29d0fcf7ccae054fee69
Renesha Matthews <reneshamatthews9@gmail.com>
2022-03-21 18:21:10 -0400

    Add scroll to top plugin

--- scripts/script.js
+++ scripts/script.js
@@ -11,7 +11,9 @@ $(document).ready(function(){
       }
     });
   });
- 	
+
+ 
+  // Contact Form
   $(document).ready(function(){
     // Add smooth scrolling to all links
     $("a").on('click', function(event) {
